4455b42d660f6c01273a296b6ec3caecc5250bf1
chanon <chanon.hongthong@gmail.com>
2021-12-25 16:21:52 +0700

    use DTO(data transfer object)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -12,11 +13,17 @@ export class TasksController {
   }
 
   @Post()
-  createTask(@Body('title') title, @Body('description') description): Task {
-    //@Body() body
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    //sample 1 @Body() body
     //console.log('body', body);
 
-    return this.taskServices.createTask(title,description);
+    //sample 2 simple way good way use DTO(Data Transfer Object)
+    //createTask(@Body('title') title, @Body('description') description): Task {}
+
+    //sample DTO
+    //createTask(@Body createTaskDto: CreateTaskDto): Task {}
+
+    return this.taskServices.createTask(createTaskDto);
 
   }
 }
